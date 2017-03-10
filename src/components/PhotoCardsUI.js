48cbd6111f37bcfa13b7b6969a3d7ea3c90134e5
Adhy <adhywiranatap@gmail.com>
2017-03-10 23:19:09 +0700

    refined heart icon and data

--- src/components/PhotoCardsUI.js
+++ src/components/PhotoCardsUI.js
@@ -14,26 +14,27 @@ const PhotoCardsUI = props => (
             resizeMode: 'cover',
           }}
         />
-      <Content style={{padding: 20, paddingTop: 10}}>
-        <Text style={{fontSize: 14, color: '#666'}}>{props.user}</Text>
-        <H2 style={{fontSize: 21, marginTop: 5}}>{props.title}</H2>
+      <Content style={{width: '100%', padding: 10, paddingTop: 10}}>
+        <Text style={{fontSize: 14, color: '#666', paddingLeft: 10}}>{props.user}</Text>
+        <H2 style={{fontSize: 21, marginTop: 5, paddingLeft: 10}}>{props.title}</H2>
         <View style={{
             height: 1,
             width: '100%',
-            backgroundColor: 'rgba(0, 0, 0, 0.1)',
+            backgroundColor: 'rgba(0, 0, 0, 0.05)',
             marginTop: 10,
-            marginBottom: 10,
         }}></View>
-        <Text style={{fontSize: 14, color: '#666'}}>{props.description}</Text>
+        <Text style={{fontSize: 14, color: '#666', padding: 10}}>
+          {props.description}
+        </Text>
 
         <TouchableOpacity style={{
-          width: 60,
-          height: 60,
-          borderRadius: 60,
+          width: 50,
+          height: 50,
+          borderRadius: 50,
           backgroundColor: '#D64541',
           position: 'absolute',
           right: 0,
-          top: 20,
+          top: 0,
           alignItems: 'center',
           justifyContent: 'center',
         }}>
