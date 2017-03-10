6b3cdace5097d118e37f39e79a662be291fcdb1b
Adhy <adhywiranatap@gmail.com>
2017-03-11 00:22:06 +0700

    updated fab

--- src/components/Fabs/MainFab.js
+++ src/components/Fabs/MainFab.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Content, Button, Icon, Fab, View } from 'native-base';
 
-const MainFab = () => (
-  <Fab
-      active={false}
-      direction="up"
-      style={{ backgroundColor: '#019875' }}
-      position="bottomRight"
-      onPress={() => {}}
-  >
-    <Icon name="camera" />
-  </Fab>
-);
+class MainFab extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    };
+  }
+
+  render() {
+    return (
+      <Fab
+          active={this.state.active}
+          direction="up"
+          style={{ backgroundColor: '#019875' }}
+          position="bottomRight"
+          onPress={() => this.setState({ active: !this.state.active })}
+      >
+        {this.state.active ? (<Icon name="remove" />) : (<Icon name="add" />) }
+        <Button style={{ backgroundColor: '#65C6BB' }}>
+          <Icon name="camera" />
+        </Button>
+        <Button style={{ backgroundColor: '#52B3D9' }}>
+          <Icon name="cloud-upload" />
+        </Button>
+        <Button style={{ backgroundColor: '#D64541' }}>
+          <Icon name="create" />
+        </Button>
+      </Fab>
+    );
+  }
+}
 
 export default MainFab;
