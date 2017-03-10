22affc67b96cdbe9c6e6c62a56de36b0fb3a94f1
Adhy <adhywiranatap@gmail.com>
2017-03-11 00:48:15 +0700

    added foods for overlays

--- src/routes/Foods/index.js
+++ src/routes/Foods/index.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { View, Image } from 'react-native';
 import { Container, Content } from 'native-base';
 
 import MainFab from '../../components/Fabs/MainFab';
+import ScrollMoreUI from '../../components/ScrollMoreUI';
 import PhotoCardsUI from '../../components/PhotoCardsUI';
 import foodsData from './foodsDB.json';
 
-const Foods = () => (
-  <Container>
-    <Content style={{ padding: 10, paddingBottom: 50}}>
-      { foodsData.foods.map(food => <PhotoCardsUI key={food.id} {...food} />)}
-    </Content>
-    <MainFab />
-  </Container>
-);
+class Foods extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      fabOverlay: false
+    }
+    this.toggleFabOverlay = this.toggleFabOverlay.bind(this);
+  }
+
+  toggleFabOverlay() {
+    this.setState({
+      fabOverlay: !this.state.fabOverlay,
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Content style={{ padding: 10, paddingBottom: 50}}>
+          { foodsData.foods.map(food => <PhotoCardsUI key={food.id} {...food} />)}
+          <ScrollMoreUI />
+        </Content>
+        { this.state.fabOverlay && (
+          <View style={{width: '100%', height: '100%', position: 'absolute', backgroundColor: 'rgba(0, 0, 0, 0.5)'}}></View>
+        )}
+        <MainFab toggleFabOverlay={this.toggleFabOverlay} />
+      </Container>
+    );
+  }
+}
 
 export default Foods;
