1aadf835eff0be38f4ab30fdc34be43ff381e8e7
Adhy <adhywiranatap@gmail.com>
2017-03-10 20:46:09 +0700

    refactored cards to photocards

--- src/routes/Places/index.js
+++ src/routes/Places/index.js
@@ -3,13 +3,13 @@ import { Image } from 'react-native';
 import { Container, Content } from 'native-base';
 
 import MainFab from '../../components/Fabs/MainFab';
-import Place from './Place';
-import photosData from './photosDB.json';
+import PhotoCardsUI from '../../components/PhotoCardsUI';
+import placesData from './placesDB.json';
 
 const Places = () => (
   <Container>
     <Content style={{ padding: 10, paddingBottom: 50}}>
-      { photosData.photos.map(place => <Place key={place.id} {...place} />)}
+      { placesData.places.map(place => <PhotoCardsUI key={place.id} {...place} />)}
     </Content>
     <MainFab />
   </Container>
