9cd02f1d3b0268ca907a0d7f5d803076ca2017c4
Adhy <adhywiranatap@gmail.com>
2017-03-11 00:08:54 +0700

    added scroll more UI on press functionality

--- src/components/ScrollMoreUI.js
+++ src/components/ScrollMoreUI.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
-import { Icon, Button } from 'native-base';
+import { Icon, Button, Spinner } from 'native-base';
 
-const ScrollMoreUI = () => (
-  <View style={{flexDirection: 'row', justifyContent: 'center', padding: 10, paddingBottom: 40}}>
-    <Button style={{
-        width: '50%',
-        backgroundColor: '#446CB3',
-        padding: 5,
-        paddingTop: 5,
-        paddingBottom: 5,
-        borderRadius: 40,
-        flexDirection: 'row',
-        justifyContent: 'center',
-    }}>
-      <Icon name="eye" style={{color: '#FFFFFF', fontSize: 15,}} />
-      <Text style={{color: '#FFFFFF', marginLeft: 5,}}>Load More</Text>
-    </Button>
-  </View>
-);
+class ScrollMoreUI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+    }
+  }
+
+  loadMore() {
+    this.setState({isLoading: true})
+  }
+
+  render() {
+    return (
+      <View style={{flexDirection: 'row', justifyContent: 'center', padding: 10, paddingBottom: 40}}>
+        <Button
+          onPress={() => { this.loadMore() }}
+          style={{
+            width: '50%',
+            backgroundColor: '#446CB3',
+            padding: 5,
+            paddingTop: 5,
+            paddingBottom: 5,
+            borderRadius: 40,
+            flexDirection: 'row',
+            justifyContent: 'center',
+        }}>
+        { this.state.isLoading ? (
+          <Spinner color='white' />
+        ) : (
+          <View style={{flexDirection: 'row', justifyContent: 'center',}}>
+            <Icon name="eye" style={{color: '#FFFFFF', fontSize: 15,}} />
+            <Text style={{color: '#FFFFFF', marginLeft: 5,}}>Load More</Text>
+          </View>
+        ) }
+        </Button>
+      </View>
+    );
+  }
+}
 
 export default ScrollMoreUI;
