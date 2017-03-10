1fcee5ef0f82a6adf0417d8876e64265d1dfc2f1
Adhy <adhywiranatap@gmail.com>
2017-03-10 20:25:21 +0700

    updated login screen

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -2,12 +2,34 @@ import React from 'react';
 import {
   View, Text, Image,
 } from 'react-native';
-import { H1, H2, Button } from 'native-base';
+import { H1, H2, Button, Spinner } from 'native-base';
 
 class AuthScreen extends React.Component {
   static navigationOptions = {
     title: 'Login'
   };
+
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      isLoading: true,
+    })
+  }
+
+  componentDidMount() {
+    const self = this;
+    setTimeout(() => {
+      this.setState({
+        isLoading: false,
+      })
+    }, 2000)
+  }
 
   render() {
     const { navigate } = this.props.navigation;
@@ -27,15 +49,31 @@ class AuthScreen extends React.Component {
         >
           <Text style={{color: '#FFFFFF', fontSize: 60, }}>HeriTage</Text>
           <H2 style={{color: '#DDDDDD'}}>Tag Yours, Visit Others</H2>
-          <View style={{margin: 20, width: '50%'}}>
-            <Button
-              block
-              style={{backgroundColor: '#019875'}}
-              onPress={() => navigate('Main')}
-            >
-              <Text style={{color: '#FFFFFF'}}>Login</Text>
-            </Button>
-          </View>
+
+          { this.state.isLoading ? ( <Spinner color='white' />) : (
+            <View style={{margin: 20, width: '60%'}}>
+              <Button
+                block
+                style={{backgroundColor: '#019875'}}
+                onPress={() => {
+                  this.setState({isLoading: true});
+                  setTimeout(() => {
+                    this.setState({isLoading: false});
+                    navigate('Main');
+                  }, 1000);
+                }}
+              >
+                <Text style={{color: '#FFFFFF'}}>Masuk</Text>
+              </Button>
+              <Button
+                block
+                style={{backgroundColor: '#F89406', marginTop: 10}}
+                onPress={() => navigate('Main')}
+              >
+                <Text style={{color: '#FFFFFF'}}>Daftar</Text>
+              </Button>
+            </View>
+          )}
         </View>
       </Image>
     );
